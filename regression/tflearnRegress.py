927c93a77489ce6158917ebd541dad40484837af
Joshuaa9088 <joshua.gf.arul@gmail.com>
2018-08-07 11:32:22 -0600

    Added model saving feature. Repaired bad tflearn log dir

--- regression/tflearnRegress.py
+++ regression/tflearnRegress.py
@@ -1,14 +1,31 @@
+import time
 import numpy as np
 import tflearn
 import matplotlib.pyplot as plt
+import os.path
 
-def train_network(x, y, n_epoch=2000, LR=0.01):
+
+def train_network(x, y, n_epoch=2000, LR=0.01, loadModel=None, retrain=False):
     input_ = tflearn.input_data(shape=[None])
     linear = tflearn.single_unit(input_)
     neural_net = tflearn.regression(linear, optimizer='sgd', loss='mean_square',
                                 metric='R2', learning_rate=LR)
 
     model = tflearn.DNN(neural_net, tensorboard_verbose=3)
+
+    if os.path.isfile(loadModel+".index"):
+        try: 
+            model.load(loadModel)
+            if retrain:
+                print("Successfully loaded old model. Forcing retrain!")
+                model.fit(x, y, n_epoch, snapshot_epoch=False)
+                return model
+
+            print("Successfully loaded old model. Skipping training...")
+            return model
+        except:
+            model.fit(x, y, n_epoch, snapshot_epoch=False)
+            return model
 
     model.fit(x, y, n_epoch, snapshot_epoch=False)
 
@@ -25,15 +42,23 @@ def getData(filename):
         data.append(line)
     return data
 
-data=getData("sampleData4.txt")
+def testRegress(dataPath, savePath, n_epochs=5000, LR=.01, retrain=False):
+    data=getData(dataPath)
 
-x=[d[0] for d in data]
-y=[d[1] for d in data]
+    x=[d[0] for d in data]
+    y=[d[1] for d in data]
+    
+    model = train_network(x, y, n_epochs, LR, savePath, retrain)
+    model.save(savePath)
 
-orModel = train_network(x, y, 10000, .03)
+    yHat = model.predict([i for i in x])
 
-yHat = orModel.predict([i for i in x])
+    plt.scatter(x, y, c="red", marker="o")
+    plt.plot(x, yHat)
+    plt.show()
 
-plt.scatter(x, y, c="red", marker="o")
-plt.plot(x, yHat)
-plt.show()
+# testRegress("sampleData.txt", "sample.model")
+# testRegress("sampleData1.txt", "sample1.model")
+# testRegress("sampleData3.txt", "sample3.model")
+testRegress("sampleData4.txt", "sample4.model", 20000)#, retrain=True)
+# testRegress("sampleData5.txt", "sample5.model", 200000)
