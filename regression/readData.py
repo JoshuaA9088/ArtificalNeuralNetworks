c11f9e902893ebccf1d1e4b373cdc47440173cd1
JoshuaA9088 <joshua.gf.arul@gmail.com>
2018-08-16 23:43:34 -0600

    Added Keras version

--- regression/readData.py
+++ regression/readData.py
@@ -1,8 +1,3 @@
-from Graphics import *
-
-
-
-
 def getData(filename):
     data=[]
     f=open(filename)
@@ -14,16 +9,3 @@ def getData(filename):
         data.append(line)
     return data
     
-data=getData("sampleData1.txt")
-
-print(data)
-
-def plotData(data):
-    color=None
-    for d in data:
-        if d[2]==1:
-            color="red"
-        else:
-            color="blue"            
-        c=Circle((d[0],d[1]),1,color)
-        c.draw(win)
